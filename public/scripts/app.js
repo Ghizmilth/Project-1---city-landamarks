adc0305bc64b9409fefa59c08d06d7a4cb80fd98
Ghizmilth <hidanerferrer@gmail.com>
2017-07-25 13:59:57 -0700

    fixed the wrond address for node to select index.js and showed our /api routhe index on browser

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,20 @@ $(document).ready(function() {
   console.log('JS is loaded');
 
 
+$.ajax({
+  method: 'GET',
+  url: '/api/cities'
+  success: renderCityList
+})
+
 
 
 })
+
+
+
+
+function renderCityList(data) {
+  console.log('data JSOn received');
+  
+}
