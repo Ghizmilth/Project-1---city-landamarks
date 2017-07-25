be62dc28fc1d2099727e1e31413cc4f5a1c17072
Ghizmilth <hidanerferrer@gmail.com>
2017-07-25 12:10:33 -0700

    created app.js file and added a console.log to know if it is loaded

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express'),
       app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 const controllers = require('./controllers');
 
