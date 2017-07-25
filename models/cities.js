d1ea5046e5bbb608d3448668d95c5f5a06169a76
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-07-25 14:36:03 -0700

    modified cities

--- models/cities.js
+++ models/cities.js
@@ -1,2 +1,15 @@
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
+
+let CitySchema = new Schema ({
+  name: String,
+  description: String,
+  population: Number,
+  size: String,
+  imgURL: String
+});
+
+let City = mongoose.model('City', CitySchema);
+
+
+module.exports = City;
