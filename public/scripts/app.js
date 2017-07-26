75ba17f46f583489fa7f836f1bf75af48a406277
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-07-25 20:09:02 -0700

    push before switch to master

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,7 +15,28 @@ $.ajax({
 
 
 
-function renderCityList(data) {
-  console.log('data JSOn received');
-  
+function renderCity(cities[1]) {
+  console.log('rendering city',city);
+
+  var cityHTML = (`
+
+    <section class="row">
+      <article class="col-sm-6">
+        <h2> San Francisco</h2>
+        <p>San Francisco, San Francisco,San Francisco,San Francisco,San Francisco,San Francisco,San Francisco,San Francisco,San Francisco,San Francisco,San Francisco,San Francisco,San Francisco,</P>
+        </article>
+        <article class="col-sm-6" id="city-image">
+          <img src="http://usa.budgettravel.ie/assets/userfiles/san-francisco-skyline-600x600.jpg"></img>
+        </article>
+  </section>
+
+  <section class="modal-footer">
+    <div class="row">
+       <article class="col-md-12" id="city-facts">
+         <p>City Facts,City Facts,City Facts,City Facts,City Facts,City Facts,City Facts,</P>
+       </article>
+    </div>
+
+`);
+
 }
