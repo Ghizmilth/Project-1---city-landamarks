b78db15aee18eee6b573e60a186a69839cafe5a7
Ghizmilth <hidanerferrer@gmail.com>
2017-08-01 09:07:38 -0700

    extra changes for heroku

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 //REQUIRE
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/project1CityLandmarks")
+//mongoose.connect("mongodb://localhost/project1CityLandmarks")
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/project1CityLandmarks" );
 
 var City = require('./cities');
 
